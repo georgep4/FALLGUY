ff85d5f3232eec14131614a286c7172ef1d245b7
georgep4 <georgep4@gmail.com>
2019-04-14 15:27:41 -0400

    FallGuy
    Ragdoll Physics

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    Vector3 targetPosition;
+    //    Update is called once per frame
+    void Update()
+    {
+      if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            targetPosition = hit.point;
+            //   this.transform.LookAt(targetPosition);
+            //   Debug.Log(hit);
+
+            Instantiate(bulletPrefab, firePoint.position, Quaternion.LookRotation(ray.direction));
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Weapon03b.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Weapon03b : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    Vector3 targetPosition;
+
+    void Start()
+    {
+        Debug.Log("Weapon03b");
+
+    }
+
+    //    Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+
+    void Shoot()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            targetPosition = hit.point;
+         //   this.transform.LookAt(targetPosition);
+         //   Debug.Log(hit);
+
+            Instantiate(bulletPrefab, firePoint.position, Quaternion.LookRotation(ray.direction));
+
+        }
+
+
+        // shooting logic
+        // Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+
+    }
+
+}
