4348f1887054aab1c87f8f4ded69e6683b7090c4
georgep4 <georgep4@gmail.com>
2019-04-22 18:15:38 -0400

    4-22BeforeDinner
    Teleportation added

---
+++ Assets/Scripts/HeadHit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadHit : MonoBehaviour
+{
+    public GameObject explosionEffect;
+    public AudioSource source;
+    public AudioClip explodeblock;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Blocks")
+        {
+            source.PlayOneShot(explodeblock);
+            GameObject impactGo = Instantiate(explosionEffect, transform.position, transform.rotation);
+            Destroy(impactGo, 2f);
+        }
+    }
+}
--- Assets/Scripts/Teleportation.cs
+++ Assets/Scripts/Teleportation.cs
@@ -6,11 +6,18 @@ public class Teleportation : MonoBehaviour
 {
     public Transform teleportTarget;
     public GameObject thePlayer;
+    public float velocity { get; internal set; }
+    public Rigidbody rb;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        thePlayer.transform.position = teleportTarget.transform.position;  
+        thePlayer.transform.position = teleportTarget.transform.position;
+        rb.velocity = Vector3.zero;
     }
 
 }
