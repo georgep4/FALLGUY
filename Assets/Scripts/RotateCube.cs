704d19d64a370ec1f57c70e802e96828f4d580c4
cjcobra <cjcobra2@gmail.com>
2019-04-24 08:49:34 -0400

    Cleaned up board, random spins

--- Assets/Scripts/RotateCube.cs
+++ Assets/Scripts/RotateCube.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class RotateCube : MonoBehaviour {
 
-    public float spinForce;
-    
+   // public float spinForce;
+    private float spinForce2;
 
 	// Use this for initialization
 	void Start () {
-		
+
+        spinForce2 = Random.Range(-80, 80);
+
+        if (spinForce2 > -20 && spinForce2 < 20)
+        {
+            spinForce2 = Random.Range(20, 80);
+        }
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(0, 0, spinForce * Time.deltaTime);
-	}
+        //   transform.Rotate(0, 0, spinForce * Time.deltaTime);
+        transform.Rotate(0, 0, spinForce2 * Time.deltaTime);
+    }
 
     public void ChangeSpin()
     {
-        spinForce = -spinForce;
+       // spinForce = -spinForce;
+        spinForce2 = -spinForce2;
     }
 
 
